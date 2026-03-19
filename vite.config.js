aa0382a5c48163ee11c724cc1f953e4ca766af6e
aljoharah4 <jojoas2004@gmail.com>
2026-03-19 17:12:06 +0300

    تت

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        puzzle: resolve(__dirname, 'modules/8-puzzle/index.html')
+        puzzle: resolve(__dirname, '8-puzzle-module.html'),
+        hanoi: resolve(__dirname, 'hanoi-module.html'),
+        hospital: resolve(__dirname, 'hospital-module.html'),
+        maze: resolve(__dirname, 'maze-module.html'),
+        pizza: resolve(__dirname, 'pizza-delivery-module.html'),
+        romania: resolve(__dirname, 'romania-map-module.html'),
+        tsp: resolve(__dirname, 'tsp-module.html'),
+        vacuum: resolve(__dirname, 'vacuum-world-module.html'),
       }
     }
   },
